56b5269aaab4ef980bede9aa2c8e8e0b0724e5bd
efrainmavi <efrainmavi@gmail.com>
2026-03-22 22:59:00 -0600

    Add multi Signed In Roles and not logged in state

--- tests/account.spec.ts
+++ tests/account.spec.ts
@@ -1,20 +1,18 @@
-import { test, expect } from '@playwright/test';
-import { beforeEach } from 'node:test';
+import { test, expect } from "@playwright/test";
 
-test.beforeEach(async ({ page }) => {
-  // open url
-  await page.goto('/my-account');
-});
+  test.beforeEach(async ({ page }) => {
+    // open url
+    await page.goto("/my-account");
+  });
 
-test.afterEach(async ({ page }) => {
-  // close page after each test
-  await page.close();
-  await page.context().close();
-});
+  test.afterEach(async ({ page }) => {
+    // close page after each test
+    await page.close();
+    await page.context().close();
+  });
 
-test.describe('My Account', () => {
-
-    /*test('Login', async ({page}) => {
+test.describe("My Account", () => {
+  /*test('Login', async ({page}) => {
         await page.locator('#username').fill('practiceuser1');
         await page.locator('#password').fill('PracticePass1!');
         await page.locator('[value="Log in"]').click();
@@ -22,13 +20,25 @@ test.describe('My Account', () => {
         await expect(page.locator("//a[text()='Log out']")).toBeVisible();
     });*/
 
-    test('Access Orders', async ({page}) => {
-        await page.locator(`li a[href*='orders']`).click();
-        await expect(page).toHaveURL(/.*orders/);
-    });
+  test("Access Orders", async ({ page }) => {
+    await page.locator(`li a[href*='orders']`).click();
+    await expect(page).toHaveURL(/.*orders/);
+  });
 
-    test('Access Downloads', async ({page})=> {
-        await page.locator(`li a[href*='downloads']`).click();
-        await expect(page).toHaveURL(/.*downloads/);
-    });
+  test("Access Downloads", async ({ page }) => {
+    await page.locator(`li a[href*='downloads']`).click();
+    await expect(page).toHaveURL(/.*downloads/);
+  });
+});
+
+test.describe("Account Page", () => {
+
+  test.use({storageState: 'notLoggedInState.json'});
+  test("Verify Login and Register Forms are visible", async ({ page }) => {
+    await page.goto('/my-account');
+    const loginForm = page.locator('form[class*="login"]');
+    const registerForm = page.locator("form[class*='register']");
+    await expect(loginForm).toBeVisible();
+    await expect(registerForm).toBeVisible();
+  });
 });
--- utils/global-setup.ts
+++ utils/global-setup.ts
@@ -2,9 +2,11 @@ import { chromium, FullConfig, expect } from '@playwright/test';
 export default async function globalSetup(config: FullConfig){
     const browser = await chromium.launch();
     const page = await browser.newPage();
+    console.log("########## Global SetUp ##########");
+    await page.goto('https://practice.sdetunicorns.com/my-account');
+    await page.context().storageState({ path: 'notLoggedInState.json' });
 
     //login
-    await page.goto('https://practice.sdetunicorns.com/my-account');
     await page.locator('#username').fill('practiceuser1');
     await page.locator('#password').fill('PracticePass1!');
     await page.locator('[value="Log in"]').click();
